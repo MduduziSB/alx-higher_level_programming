bc171f1b24a1d747a0ee3e1118e5473d5552c7b0
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-06-06 05:23:40 -0700

    task 2

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,7 @@ if number > 0:
     if last and last <= 5:
         print(f"{last:d} and is less than 6 and not 0")
     if number % 10 and number % 10 > 5:
-        print(f"{last:d} and is greater than 5 and not 0")
+        print(f"{last:d} and is greater than 5")
     if number % 10 == 0:
         print(f"{last:d} and is 0")
 else:
