dd8277f3b98b47434c6df1a27a6ac9b459e58464
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-06-06 02:42:27 -0700

    tasks 0 and 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number >= 0:
+    last = number % 10
+else:
+    last = (-1 * number) % 10
+print(f"Last digit of {number:d} is", end=" ")
+if number > 0:
+    if last and last <= 5:
+        print(f"{last:d} and is less than 6 and not 0")
+    if number % 10 and number % 10 > 5:
+        print(f"{last:d} and is greater than 5 and not 0")
+    if number % 10 == 0:
+        print(f"{last:d} and is 0")
+else:
+    if number % 10:
+        print(f"-{last:d} and is less than 6 and not 0")
+    else:
+        print(f"{last:d} and is 0")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+Python project(0x01) on if/else, loops and functions
