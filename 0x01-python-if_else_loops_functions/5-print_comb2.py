62ebf275bed1a12634a8a6314eaaa2b34df9771b
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-06-06 06:11:59 -0700

    5-print_com2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i < 10:
+    if i < 99:
         print("{0:02d}".format(i), end=", ")
     else:
-        if i < 99:
-            print("{}".format(i), end=", ")
-        else:
-            print("{}".format(i))
+        print("{}".format(i))
