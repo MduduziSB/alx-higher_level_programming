4646fd5ca923225228a834e69ec88837a5346f9f
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-06-08 12:06:28 -0700

    task4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import py_compile
+import marshal
+
+py_compile.compile('hidden_4.py')
+code = open('hidden_4.pyc', 'rb').read()
+
+names = []
+for const in marshal.load(code)['co_consts']:
+    if isinstance(const, str) and not const.startswith('__'):
+        names.append(const)
+
+for name in sorted(names):
+    print(name)
