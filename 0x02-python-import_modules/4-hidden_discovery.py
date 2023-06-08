2086fa025b7bc74e10c1dec866ffb67de266d387
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-06-08 12:11:08 -0700

    4-hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-import py_compile
-import marshal
+if __name__ == "__main__":
+    import py_compile
+    import marshal
 
-py_compile.compile('hidden_4.py')
-code = open('hidden_4.pyc', 'rb').read()
-
-names = []
-for const in marshal.load(code)['co_consts']:
-    if isinstance(const, str) and not const.startswith('__'):
-        names.append(const)
-
-for name in sorted(names):
-    print(name)
+    py_compile.compile('hidden_4.py')
+    code = open('hidden_4.pyc', 'rb').read()
+    names = []
+    for const in marshal.load(code)['co_consts']:
+        if isinstance(const, str) and not const.startswith('__'):
+            names.append(const)
+    for name in sorted(names):
+        print(name)
