1c27d9d14dba64fad1b46bfc39aec3179631c72e
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-06-08 03:45:40 -0700

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     if nargs == 1:
         print("{} arguments.".format(nargs - 1))
     else:
-        print("{} arguments:".format(nargs - 1))
+        if nargs  == 2:
+            print("{} argument:".format(nargs - 1))
+        else:
+            print("{} arguments:".format(nargs - 1))
         for i in range(1, nargs):
             print("{}: {}".format(i, sys.argv[i]))
