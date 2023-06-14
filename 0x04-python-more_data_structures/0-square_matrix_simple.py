714880289a3b31b3503ecb58930f9bf9b0fd1fdd
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-06-14 14:48:49 -0700

    square_matrix_simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is not None:
-        new_matrix = [[0 for r in range(len(matrix))]
-                      for c in range(len(matrix[0]))]
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                new_matrix[i][j] = matrix[i][j]**2
-        return new_matrix
-    return None
+    n_matrix = matrix.copy()
+    for r in range(len(matrix)):
+        n_matrix[r] = list(map(lambda y: y**2, matrix[r]))
+    return n_matrix
