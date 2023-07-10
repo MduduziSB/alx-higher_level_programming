bc8d1c18fea8e27c1e5dcffd86a0314a0ba08e7d
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-07-10 09:01:51 -0700

    corrected task4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Module that determines whether an object is an instance of a class that"""
+"""inherited (directly or indirectly) from the specified class"""
+
+
 def inherits_from(obj, a_class):
     """Tells whether or not an object is an instance"""
     """or herited from the spacified type"""
+
     if type(obj) == a_class or isinstance(type(obj), a_class):
         return True
     return False
