e3b04d21c488037149a0d8ebfe0884b427bd37d5
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-07-10 10:45:43 -0700

    task0(lookup)

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,6 +7,6 @@ def inherits_from(obj, a_class):
     """Tells whether or not an object is an instance"""
     """or herited from the spacified type"""
 
-    if type(obj) == a_class or isinstance(type(obj), a_class):
+    if issubclass(type(obj), a_class):
         return True
     return False
