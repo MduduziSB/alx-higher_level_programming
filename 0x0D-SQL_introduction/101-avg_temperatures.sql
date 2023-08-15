f798e470fab050613ceb315b8209cac891bb8eca
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-08-15 14:27:04 -0700

    Task 100-103

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,6 @@
 -- The script displays the average temperature
 --  by city ordered by temperature (descending)
-SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-ORDER BY avg_temp DESC
+SELECT city, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,9 @@
 -- The script displays the top 3 of cities temperature
 -- during July and August
 -- ordered by temperature (descending)
-SELECT `city`, `value` AS `avg_temp`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 WHERE `month` IN (7,8)
+GROUP BY `city`
 ORDER BY `avg_temp` DESC
 LIMIT 3;
