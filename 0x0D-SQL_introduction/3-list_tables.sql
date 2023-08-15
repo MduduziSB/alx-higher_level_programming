554a6de9b96ccb83992e0d5a7aecc0e2e031032c
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-08-15 09:33:49 -0700

    tasks 0-1 and 3

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- This script lists all tables of a database in my MySQL server
+mysql -e "USE $database_name; SHOW TABLES;" -u your_username -p
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,1 @@
+ALX First project on SQL(0x0D. SQL - Introduction)
