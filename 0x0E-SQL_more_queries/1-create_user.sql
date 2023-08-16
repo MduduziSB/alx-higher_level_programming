71921dc1e4b9a351f49b0a41c788af9a013e8f00
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-08-16 08:17:51 -0700

    changes on Task7-9

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,5 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to the user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to apply changes
+GRANT ALL PRIVILEGES ON classicmodels.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
