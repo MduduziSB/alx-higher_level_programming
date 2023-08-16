cadc22ac6298e1d1229a2bda8dd7af925ded2c49
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-08-16 12:58:30 -0700

    some changes on Task11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,13 +1,6 @@
 --- This script lists all shows contained
 -- In the database hbtn_0d_tvshows
-SELECT
-  ts.title,
-  IFNULL(GROUP_CONCAT(tsg.genre_id ORDER BY tsg.genre_id ASC), 'NULL') AS genre_ids
-FROM
-  tv_shows ts
-LEFT JOIN
-  tv_show_genres tsg ON ts.id = tsg.show_id
-GROUP BY
-  ts.id, ts.title
-ORDER BY
-  ts.title ASC, genre_ids;
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
