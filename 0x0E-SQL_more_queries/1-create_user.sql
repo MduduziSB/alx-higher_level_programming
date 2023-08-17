ac063cda21fae1db0faaa7f3fdad571051047e4a
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-08-17 05:10:18 -0700

    Corrected Task1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- Create user if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
 
 -- Grant all privileges to the user
-GRANT ALL ON classicmodels.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON `classicmodels`.* TO `user_0d_1`@`localhost`;
 FLUSH PRIVILEGES;
