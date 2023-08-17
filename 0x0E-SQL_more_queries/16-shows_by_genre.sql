f4a6a266aa2dff1ba626ec10bfeec50535cac769
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-08-17 04:52:43 -0700

    Corrected Task 16

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,4 +6,4 @@ SELECT ts.title, tg.name
 FROM tv_shows AS ts
 LEFT JOIN tv_show_genres AS tsg ON ts.id = tsg.show_id
 LEFT JOIN tv_genres AS tg ON tsg.genre_id = tg.id
-ORDER BY ts.title, genres ASC;
+ORDER BY ts.title, tg.name ASC;
