c64a358bcfb9f84787dc18d6aae701fed027e52b
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-09-12 20:57:38 -0700

    Task3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/* class Rectangle definition */
+
+class Rectangle {
+	constructor (w, h) {
+		if (w > 0 && h > 0) {
+			this.width = w;
+			this.height = h;
+		}
+	}
+	
+	print () {
+		let idy;
+		for (idy = 0; idy < this.height; idy++) {
+			console.log('X'.repeat(this.width));
+		}
+	}
+}
+module.exports = Rectangle;
+
