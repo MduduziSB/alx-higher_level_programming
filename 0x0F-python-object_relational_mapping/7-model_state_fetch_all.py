3e4c6b8d0397cb9795a9062cef9fad2441453aef
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-09-22 01:52:26 -0700

    Task7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""definition of a State and an instance Base = declarative_base()"""
+
+if __name__ == "__main__":
+    from model_state import Base, State
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    arg = sys.argv
+    if len(argv) > 3:
+        var = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        engine = create_engine(var.format(arg[1], arg[2], arg[2]))
+        Session = sessionmaker(bind=engine)
+
+        Base.metadata.create_all(engine)
+
+        session = Session()
+
+        states = session.query(State).order_by(State.id).all()
+        for state in states:
+            print("{}: {}".format(state.id, state.name))
+
+        session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
 """definition of a State and an instance Base = declarative_base()"""
-
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
-db_user = 'your_username'
-db_password = 'your_password'
-db_host = 'localhost'
-db_port = 3306
-db_name = 'your_database_name'
-db_url = f'mysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
-engine = create_engine(db_url)
-Session = sessionmaker(bind=engine)
-session = Session()
 
-Base = declarative_base()
+mdata = MetaData()
+
+Base = declarative_base(metadata=mdata)
 
 
 class State(Base):
@@ -25,7 +15,3 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-
-Base.metadata.create_all(engine)
-session.close
