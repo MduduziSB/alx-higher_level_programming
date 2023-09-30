557a19123bbd1540aa777c55ca6b6875fc283581
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-09-29 19:42:24 -0700

    Task100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
+    params = {'per_page': 10}
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
