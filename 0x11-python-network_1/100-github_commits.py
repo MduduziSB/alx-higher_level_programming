1faeae253e2745c81477a7363330c615e9af2c9c
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-09-29 20:01:52 -0700

    Modified Task100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,7 +8,7 @@ import sys
 
 
 if __name__ == "__main__":
-    url = f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits"
+    url = f"https://api.github.com/repos/{sys.argv[2]}/{sys.argv[1]}/commits"
     params = {'per_page': 10}
     response = requests.get(url, params=params)
 
