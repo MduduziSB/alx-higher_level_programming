3cd9017e6fbf49e1aa4e23e1fd2d30ed73b0a60a
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-10-24 12:00:19 -0700

    task100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+
+    const characterUrls = movieData.characters;
+
+    characterUrls.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (!charError && charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        } else {
+          console.error(charError);
+        }
+      });
+    });
+  } else {
+    console.error(error);
+  }
+});
